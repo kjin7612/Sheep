857a2c3dae49f9102b54494afbae858e313d6443
Ealry Connect <hyeonjin.kim@earlyconnect.com>
2021-09-05 13:29:14 -0400

    first commit

---
+++ sheep.js
@@ -0,0 +1,31 @@
+export class Sheep{
+    constructor(img, stageWidth){
+        this.img = img;
+
+        // 프래임 정의
+        this.totalFrame = 8;
+        this.curFrame = 0;
+
+        this.imgWidth = 360;
+        this.imgHeight = 300;
+
+        this.sheepWidth = 180;
+        this.sheepHeight = 150;
+
+        this.sheepWidthHalf = this.sheepWidth / 2;
+        this.x = stageWidth + this.sheepWidth;
+        this.y = 0;
+        this.speed = Math.random() * 2 + 1;
+
+        this.fps = 24;
+        this.fpsTime = 1000 / this.fps;
+    }
+
+    draw(ctx, t, dots){
+        this.animate(ctx, dots);
+    }
+
+    animate(ctx, dots){
+
+    }
+}
